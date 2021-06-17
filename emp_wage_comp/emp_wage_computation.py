83cc8b5b5b49e01d68dbc60652786c095ba6c684
testForMe-Now <abcodehash@gmail.com>
2021-06-17 21:04:03 +0530

    ability to calculate daily wage is added

--- emp_wage_comp/emp_wage_computation.py
+++ emp_wage_comp/emp_wage_computation.py
@@ -2,24 +2,40 @@ import random
 
 
 class Employee:
-    def __init__(self, emp_name, emp_id, department):
+    def __init__(self, emp_name, emp_id, department,is_fulltime):
         self.emp_name = emp_name
         self.emp_id = emp_id
         self.department = department
+        self.work_type = is_fulltime
 
     def check_emp_present_or_absent(self):
         is_present = 1
         random_num = random.randint(0, 1)
         if random_num == is_present:
-            print(self.emp_name+" "+"present")
+            return True
         else:
-            print(self.emp_name+" "+"absent")
+            return False
+
+    def calculate_daily_wage(self):
+        status = self.check_emp_present_or_absent()
+        if status:
+            emp_hrs = 0
+            emp_wage = 0
+            rate_per_hrs = 20
+            if self.work_type == 1:
+                emp_hrs = 8
+            else:
+                emp_hrs = 0
+            emp_wage = emp_hrs * rate_per_hrs
+            print("daily Wage"+" "+str(emp_wage))
+        else:
+            print("employee is absent")
 
 
 def main():
     print("welcome to employee wage computation")
-    employee = Employee("Andra", 1, "IT")
-    employee.check_emp_present_or_absent()
+    employee = Employee("Andra", 1, "IT",1)
+    employee.calculate_daily_wage()
 
 
 if __name__ == '__main__':
