9e193e3ad37261f58fc3e9fe1dfc8453dfb61e90
testForMe-Now <abcodehash@gmail.com>
2021-06-17 21:21:11 +0530

    ability to calculate partime wage is added

--- emp_wage_comp/emp_wage_computation.py
+++ emp_wage_comp/emp_wage_computation.py
@@ -2,16 +2,16 @@ import random
 
 
 class Employee:
-    def __init__(self, emp_name, emp_id, department,is_fulltime):
+    def __init__(self, emp_name, emp_id, department, form, check):
         self.emp_name = emp_name
         self.emp_id = emp_id
         self.department = department
-        self.work_type = is_fulltime
+        self.work_type = form
+        self.is_present = check
 
     def check_emp_present_or_absent(self):
-        is_present = 1
         random_num = random.randint(0, 1)
-        if random_num == is_present:
+        if random_num == self.is_present:
             return True
         else:
             return False
@@ -24,6 +24,8 @@ class Employee:
             rate_per_hrs = 20
             if self.work_type == 1:
                 emp_hrs = 8
+            elif self.work_type == 2:
+                emp_hrs = 4
             else:
                 emp_hrs = 0
             emp_wage = emp_hrs * rate_per_hrs
@@ -34,7 +36,8 @@ class Employee:
 
 def main():
     print("welcome to employee wage computation")
-    employee = Employee("Andra", 1, "IT",1)
+    work_type_input = input("enter '1' if full_time else '2' for part_time ")
+    employee = Employee("Andra", 1, "IT", int(work_type_input), 1)
     employee.calculate_daily_wage()
 
 
